5bcf5921f27f3d836d2f7cfa59de2f8a4ff65c6e
MOOMENTE <153730674+MOOMENTE@users.noreply.github.com>
2025-09-19 12:51:23 +0300

    Update apps.py

--- yatube_api/posts/apps.py
+++ yatube_api/posts/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
-
-    def ready(self):
-        # Import signals so raw passwords get hashed automatically.
-        from . import signals  # noqa: F401
